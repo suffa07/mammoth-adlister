7dd87b9d504e82354dfad4da2e2c9bcff44c58b5
Luis Montealegre <montealegreluis@gmail.com>
2017-06-06 09:45:51 -0500

    Add code to the hello world servlet.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,14 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter output = res.getWriter();
+        output.println("<h1>Hello world</h1>");
     }
 }
