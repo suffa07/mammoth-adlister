b135fba94bb7146b51125ac0c06b7f85b14f7b5f
Luis Montealegre <montealegreluis@gmail.com>
2017-06-06 09:58:05 -0500

    Add example on how to use query string parameters.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -9,6 +9,14 @@ import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         PrintWriter output = res.getWriter();
-        output.println("<h1>Hello world</h1>");
+
+        String aCase = req.getParameter("case");  // if case is not present, it will be null
+        if ("upper".equalsIgnoreCase(aCase)) {
+            output.println("<h1>Hello world</h1>".toUpperCase());
+        } else if ("lower".equalsIgnoreCase(aCase)) {
+            output.println("<h1>Hello world</h1>".toLowerCase());
+        } else {
+            output.println("<h1>Hello world</h1>");
+        }
     }
 }
